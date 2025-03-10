738259813c1e82b68e5e4c1e8d4c85795353df45
Fede Meson <federico.meson@alu.frt.utn.edu.ar>
2025-03-09 22:27:19 -0300

    Add server authentication middleware

---
+++ server/src/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token no proporcionado' });
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: 'Token inválido o expirado' });
+  }
+};
+
+module.exports = {
+  authenticateToken
+};
